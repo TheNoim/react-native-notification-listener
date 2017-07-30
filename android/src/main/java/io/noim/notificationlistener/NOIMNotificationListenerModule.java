355e6d894d0c622a26a27f22bf88008e18c9a6fc
Nils <magiccraft.nitrado@gmail.com>
2017-07-30 15:59:37 +0200

    :bug: idfk

--- android/src/main/java/io/noim/notificationlistener/NOIMNotificationListenerModule.java
+++ android/src/main/java/io/noim/notificationlistener/NOIMNotificationListenerModule.java
@@ -3,11 +3,11 @@ package io.noim.notificationlistener;
 
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.support.annotation.Nullable;
 
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
@@ -15,9 +15,13 @@ import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
-import java.util.ArrayList;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Iterator;
-import java.util.List;
 
 
 public class NOIMNotificationListenerModule extends ReactContextBaseJavaModule {
@@ -28,7 +32,7 @@ public class NOIMNotificationListenerModule extends ReactContextBaseJavaModule {
     public NOIMNotificationListenerModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        this.h = new Hook();
+        this.h = new Hook(reactContext);
     }
 
     @Override
@@ -36,34 +40,43 @@ public class NOIMNotificationListenerModule extends ReactContextBaseJavaModule {
         return "NOIMNotificationListener";
     }
 
-    public void sendEvent(ReactContext reactContext, String eventName, @Nullable WritableMap params) {
-        reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
-    }
-
     class Hook extends NotificationListenerService {
+
+        private final ReactApplicationContext reactContext;
+
+        public Hook(ReactApplicationContext reactContext) {
+            this.reactContext = reactContext;
+        }
+
+        public void sendEvent(ReactContext reactContext, String eventName, @Nullable WritableMap params) {
+            reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class).emit(eventName, params);
+        }
 
         @Override
         public void onNotificationPosted(StatusBarNotification sbn) {
-            sendEvent(reactContext, "received");
             JSONObject notification = new JSONObject();
             try {
+                sendEvent(reactContext, "received", convertJsonToMap(new JSONObject()));
                 notification.put("title", sbn.getNotification().extras.get("android.title"));
                 notification.put("pkg", sbn.getPackageName());
                 notification.put("text", sbn.getNotification().extras.get("android.text"));
                 notification.put("textLines", sbn.getNotification().extras.get("android.textLines"));
                 sendEvent(reactContext, "notification", convertJsonToMap(notification));
             } catch (JSONException e) {
-                sendEvent(reactContext, "error");
                 e.printStackTrace();
             }
         }
 
         @Override
         public void onNotificationRemoved(StatusBarNotification sbn) {
-            sendEvent(reactContext, "removed");
+            try {
+                sendEvent(reactContext, "removed", convertJsonToMap(new JSONObject()));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
 
-        private static WritableMap convertJsonToMap(JSONObject jsonObject) throws JSONException {
+        private WritableMap convertJsonToMap(JSONObject jsonObject) throws JSONException {
             WritableMap map = new WritableNativeMap();
 
             Iterator<String> iterator = jsonObject.keys();
@@ -89,7 +102,7 @@ public class NOIMNotificationListenerModule extends ReactContextBaseJavaModule {
             return map;
         }
 
-        private static WritableArray convertJsonToArray(JSONArray jsonArray) throws JSONException {
+        private WritableArray convertJsonToArray(JSONArray jsonArray) throws JSONException {
             WritableArray array = new WritableNativeArray();
 
             for (int i = 0; i < jsonArray.length(); i++) {
@@ -113,7 +126,7 @@ public class NOIMNotificationListenerModule extends ReactContextBaseJavaModule {
             return array;
         }
 
-        private static JSONObject convertMapToJson(ReadableMap readableMap) throws JSONException {
+        private JSONObject convertMapToJson(ReadableMap readableMap) throws JSONException {
             JSONObject object = new JSONObject();
             ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
             while (iterator.hasNextKey()) {
@@ -142,7 +155,7 @@ public class NOIMNotificationListenerModule extends ReactContextBaseJavaModule {
             return object;
         }
 
-        private static JSONArray convertArrayToJson(ReadableArray readableArray) throws JSONException {
+        private JSONArray convertArrayToJson(ReadableArray readableArray) throws JSONException {
             JSONArray array = new JSONArray();
             for (int i = 0; i < readableArray.size(); i++) {
                 switch (readableArray.getType(i)) {
@@ -169,4 +182,5 @@ public class NOIMNotificationListenerModule extends ReactContextBaseJavaModule {
         }
 
     }
+
 }
